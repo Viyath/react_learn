0f9e84b1ac305b8251435c82565bd6e4315ef430
Viyath <31150964+Viyath@users.noreply.github.com>
2020-10-10 22:41:23 +1100

    Phase 1 - To do App Having 4 checkboxes with placeholders as the first phase of the to do app.

--- components/App.js
+++ components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import Nav from "../components/Navbar";
-import Main from "../components/MainContent";
-import Footer from "../components/Footer";
 
 function App() {
   return (
     <div>
-      <h1>Component exercise</h1>
-      <Nav />
-      <Main />
-      <Footer />
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
     </div>
   );
 }
