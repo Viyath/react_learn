bbdad1999ed5c0b342df9b8a0cc5151a75e11bba
Viyath <31150964+Viyath@users.noreply.github.com>
2020-10-10 19:04:52 +1100

    Component Exercise // Set up the React app from scratch // Render an App component (defined in a separate file) // Inside App, render:   // 1. A Navbar component   // 2. A MainContent component   // 3. A Footer component

--- components/App.js
+++ components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Nav from "../components/Navbar";
+import Main from "../components/MainContent";
 import Footer from "../components/Footer";
 
 function App() {
   return (
     <div>
-      <h1>Hello World </h1>
+      <h1>Component exercise</h1>
+      <Nav />
+      <Main />
       <Footer />
     </div>
   );
--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <p>This is footer Element</p>;
+  return (
+    <div>
+      <p>This is the Footer</p>
+    </div>
+  );
 }
 
 export default Footer;
