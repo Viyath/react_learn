4f2f7107f929b00868e23e09db89a30f3110584d
Viyath <31150964+Viyath@users.noreply.github.com>
2020-10-11 13:00:06 +1100

    styling navbar component Import the css style sheet className is the key to link the class.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import '../src/styles.css';
 
 function NavBar() {
   return (
     <div>
-      <h1>This is the Navigation Bar</h1>
+      <h1 className="navbar">This is the Navigation Bar</h1>
     </div>
   );
 }
