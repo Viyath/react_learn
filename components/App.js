727626829dfd2d556bb8b6666b39f9c4667e1483
Viyath <31150964+Viyath@users.noreply.github.com>
2020-10-11 18:25:06 +1100

    Making Complex JS This will simply shows the Greeting based on the local time

--- components/App.js
+++ components/App.js
@@ -4,13 +4,22 @@ import Main from "../components/MainContent";
 import Footer from "../components/Footer";
 
 function App() {
-  const fName = "konga"
-  const lName = "anta"
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+  
+if (hours < 12) {
+  timeOfDay = "morning"
+} else if (hours >= 12 && hours < 17){  
+  timeOfDay = "afternoon"
+} else {
+  timeOfDay = "night"
+}
 
   return (
     <div>
-      <h1>Hello {`${fName} ${lName}`} !</h1>
-      <Nav />{fName + " " + lName}
+      <h1>Good {timeOfDay} !</h1>
+      <Nav />
       <Main />
       <Footer />
     </div>
